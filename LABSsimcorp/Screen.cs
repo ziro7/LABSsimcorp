54bdbec2132402cfb53cee422024331d5b5ad1aa
Jacob Kjærgaard <ziro776@gmail.com>
2019-08-05 20:23:51 +0200

    Setting up

---
+++ LABSsimcorp/Screen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABSsimcorp {
+        public class Screen {
+            public double SizeInInches { get; set; }
+            public int PixelCount { get; set; }
+
+            public Screen(double sizeInInches, int pixelCount) {
+                SizeInInches = sizeInInches;
+                PixelCount = pixelCount;
+            }
+        }
+    }
