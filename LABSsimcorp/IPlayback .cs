bf185df7ca3994569997f13c0145e7bb4d21a9c9
Jacob Kjærgaard <ziro776@gmail.com>
2019-08-06 10:51:47 +0200

    Task 2.1 Done

---
+++ LABSsimcorp/IPlayback .cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABSsimcorp {
+    public interface IPlayback {
+
+        void Play();
+    }
+}
---
+++ LABSsimcorp/Speakers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABSsimcorp {
+    class Speakers : IPlayback,IBluetoothConnect {
+
+        public bool UseingBluetooth { get; set; }
+
+        public Speakers(bool useingBluetooth) {
+            UseingBluetooth = useingBluetooth;
+            if (UseingBluetooth) {
+                Pairring();
+            }
+        }
+
+        public void Pairring() {
+            Console.WriteLine("Bluetooth connected");
+        }
+
+        public void Play() {
+            Console.WriteLine("Plays on the " + typeof(Speakers));
+        }
+    }
+}
