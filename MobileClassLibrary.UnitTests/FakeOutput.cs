ab803614b5a381bd2f0aeb2cc9a67d2c2ce3b883
Jacob Kjærgaard <ziro776@gmail.com>
2019-08-06 22:29:02 +0200

    Prep for Task 2.5

---
+++ MobileClassLibrary.UnitTests/FakeOutput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LABSsimcorp;
+
+namespace MobileClassLibrary.UnitTests {
+    class FakeOutput : IOutput {
+        public string WriteText { get; set; }
+        public string WriteLineText { get; set; }
+
+        public void Write(string text) {
+            WriteText = text;
+        }
+
+        public void WriteLine(string text) {
+            WriteLineText = text;
+        }
+    }
+}
