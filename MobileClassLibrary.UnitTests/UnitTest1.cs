6a87707684ff20ee420113bba0627e67e6fe1933
Jacob Kjærgaard <ziro776@gmail.com>
2019-08-07 11:15:21 +0200

    Task 2.5 Done

--- MobileClassLibrary.UnitTests/UnitTest1.cs
+++ MobileClassLibrary.UnitTests/UnitTest1.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using LABSsimcorp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MobileClassLibrary.UnitTests {
     [TestClass]
     public class UnitTest1 {
         [TestMethod]
-        public void TestMethod1() {
+        public void CreatePhone_Headphones_OutputValidate() {
+
+            //arrange
+            var output = new FakeOutput();
+            var mobile = new MobilePhone(Model.Iphone8,output);
+            
+            //act
+            mobile.InsertEquipmentInJackStick(new Headphones(output));
+            mobile.AudioInJackStik.Play(new object());
+
+            //assert
+            string outputFromMobile = output.WriteLineText;
+            Assert.AreEqual(outputFromMobile, "Plays on the " + typeof(Headphones));
         }
     }
 }
