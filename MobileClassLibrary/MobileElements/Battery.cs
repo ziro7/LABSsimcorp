b0fa9c6d1e16e5df393439745e7a429a9a5d341d
Jacob Kjærgaard <ziro776@gmail.com>
2019-08-19 14:11:26 +0200

    Task 5.2 Done (i think - hard to check before task 5.3)

--- MobileClassLibrary/MobileElements/Battery.cs
+++ MobileClassLibrary/MobileElements/Battery.cs
@@ -1,11 +1,49 @@
-﻿namespace LabsSimcorp {
+﻿using System;
+using System.Threading;
+
+namespace LabsSimcorp {
     public class Battery {
 
         public double Size { get; set; }
-        public bool IsCharging { get; set; }    
+        public bool IsCharging { get; set; }
+        public double PercentageCharged { get; set; }
+
+        Thread dischargeThread;
+        Thread chargeThread;
+
+        private static object charging = new object();
 
         public Battery(double size) {
             Size = size;
+            IsCharging = false;
+            PercentageCharged = 1.00;
+            dischargeThread = new Thread(new ThreadStart(Discharge));
+            dischargeThread.Start();
+            chargeThread = new Thread(new ThreadStart(Charge));
+            chargeThread.Start();
+
+        }
+
+        private void Charge() {
+
+            // Lock the charging as long as IsCharing is true - checks every sec.
+            lock (charging) {
+                while (IsCharging) {
+                    PercentageCharged += PercentageCharged + 0.01;
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        private void Discharge() {
+
+            // Locking the thread while not charging.
+            lock (charging) {
+                while (!IsCharging) {
+                    PercentageCharged -= PercentageCharged - 0.01;
+                    Thread.Sleep(1000);
+                }
+            }
         }
     }
 }
